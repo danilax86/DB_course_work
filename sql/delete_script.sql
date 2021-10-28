828ac7396a523b53cea422a66d9288e8422d4c73
ckafff <borisnovoselov602@gmail.com>
2021-10-29 01:18:49 +0300

    delete_script.sql

---
+++ sql/delete_script.sql
@@ -0,0 +1,3 @@
+DROP TABLE animals_type, plants_type, world_part CASCADE;
+DROP TABLE animals_sort, plants_sort CASCADE;
+DROP TABLE animals, plants, animals_location, plants_location, animals_plants;

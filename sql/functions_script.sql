18c442b429ef35b34f4df13508afc48bc0dfffd0
Danila Gorelko <danila@danilax86.ru>
2021-10-31 16:24:39 +0300

    fix functions

--- sql/functions_script.sql
+++ sql/functions_script.sql
@@ -67,215 +67,201 @@ $$ language plpgsql;
 
 drop function get_animal_sort_description_by_sort_id(integer);
 create or replace function get_animal_sort_description_by_sort_id(sort_id integer)
-returns text as
+returns table (sort integer, type integer, name text, description text) as
 $$
-    declare
-        name text := (select animals_sort.name from animals_sort where animals_sort_id = sort_id);
-        description text := (select animals_sort.description from animals_sort where animals_sort_id = sort_id);
     begin
-        return name || '; ' ||  description;
+        return query select * from animals_sort where animals_sort_id = sort_id;
     end;
 $$ language plpgsql;
 
 
 drop function get_animal_sort_description_by_animal_id(integer);
 create or replace function get_animal_sort_description_by_animal_id(animal_id integer)
-returns text as
+returns table (sort integer, type integer, name text, description text) as
 $$
-    declare
-        sort_id integer := (select animals.animals_sort_id from animals where animals.animals_id = animal_id);
-        description text := (select get_animal_sort_description_by_sort_id(sort_id));
     begin
-        return description;
+        return query select animals_sort.animals_sort_id,
+               animals_sort.animals_type_id,
+               animals_sort.name,
+               animals_sort.description
+            from animals_sort
+            inner join animals on animals_sort.animals_sort_id = animals.animals_sort_id
+        where animals.animals_id = animal_id;
     end;
 $$ language plpgsql;
 
 
 drop function get_animal_sort_description_by_animal_name(text);
 create or replace function get_animal_sort_description_by_animal_name(animal_name text)
-returns text as
+returns table (sort integer, type integer, name text, description text) as
 $$
-    declare
-        sort_id integer := (select animals.animals_sort_id from animals where animals.name = animal_name);
-        description text := (select get_animal_sort_description_by_sort_id(sort_id));
     begin
-        return description;
+        return query select animals_sort.animals_sort_id,
+               animals_sort.animals_type_id,
+               animals_sort.name,
+               animals_sort.description
+            from animals_sort
+            inner join animals on animals_sort.animals_sort_id = animals.animals_sort_id
+        where animals.name = animal_name;
     end;
 $$ language plpgsql;
 
 ------------------------------------------------------------------------------------------------------------------------
 drop function get_plant_sort_description_by_sort_id(integer);
 create or replace function get_plant_sort_description_by_sort_id(sort_id integer)
-returns text as
+returns table (sort integer, type integer, name text, description text) as
 $$
-    declare
-        name text := (select plants_sort.name from plants_sort where plants_sort_id = sort_id);
-        description text := (select plants_sort.description from plants_sort where plants_sort_id = sort_id);
     begin
-        return name || '; ' ||  description;
+        return query select * from plants_sort where plants_sort_id = sort_id;
     end;
 $$ language plpgsql;
 
 
 drop function get_plant_sort_description_by_plant_id(integer);
 create or replace function get_plant_sort_description_by_plant_id(plant_id integer)
-returns text as
+returns table (sort integer, type integer, name text, description text) as
 $$
-    declare
-        sort_id integer := (select plants.plants_sort_id from plants where plants.plants_id = plant_id);
-        description text := (select get_plant_sort_description_by_sort_id(sort_id));
     begin
-        return description;
+        return query select plants_sort.plants_sort_id,
+               plants_sort.plants_type_id,
+               plants_sort.name,
+               plants_sort.description
+            from plants_sort
+            inner join plants on plants_sort.plants_sort_id = plants.plants_sort_id
+        where plants.plants_id = plant_id;
     end;
 $$ language plpgsql;
 
 
 drop function get_plant_sort_description_by_plant_name(text);
 create or replace function get_plant_sort_description_by_plant_name(plant_name text)
-returns text as
+returns table (sort integer, type integer, name text, description text) as
 $$
-    declare
-        sort_id integer := (select plants.plants_sort_id from plants where plants.name = plant_name);
-        description text := (select get_plant_sort_description_by_sort_id(sort_id));
     begin
-        return description;
+        return query select plants_sort.plants_sort_id,
+               plants_sort.plants_type_id,
+               plants_sort.name,
+               plants_sort.description
+            from plants_sort
+            inner join plants on plants_sort.plants_sort_id = plants.plants_sort_id
+        where plants.name = plant_name;
     end;
 $$ language plpgsql;
 
 ------------------------------------------------------------------------------------------------------------------------
 drop function get_animal_type_by_type_id(integer);
 create or replace function get_animal_type_by_type_id(type_id integer)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select animals_type.name from animals_type where animals_type_id = type_id);
     begin
-        return name;
+        return query select * from animals_type where animals_type_id = type_id;
     end;
 $$ language plpgsql;
 
 
 drop function get_animal_type_by_animal_id(integer);
 create or replace function get_animal_type_by_animal_id(animal_id integer)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select animals_type.name from animals_type
+    begin
+        return query select animals_type.animals_type_id, animals_type.name from animals_type
         inner join animals_sort on animals_type.animals_type_id = animals_sort.animals_type_id
         inner join animals on animals.animals_sort_id = animals_sort.animals_sort_id
-        where  animals.animals_id = animal_id);
-    begin
-        return name;
+        where animals.animals_id = animal_id;
     end;
 $$ language plpgsql;
 
 
 drop function get_animal_type_by_animal_name(text);
 create or replace function get_animal_type_by_animal_name(animal_name text)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select animals_type.name from animals_type
+    begin
+        return query select animals_type.animals_type_id, animals_type.name from animals_type
         inner join animals_sort on animals_type.animals_type_id = animals_sort.animals_type_id
         inner join animals on animals.animals_sort_id = animals_sort.animals_sort_id
-        where  animals.name = animal_name);
-    begin
-        return name;
+        where  animals.name = animal_name;
     end;
 $$ language plpgsql;
 
 
 drop function get_animal_type_by_animal_sort_id(integer);
 create or replace function get_animal_type_by_animal_sort_id(sort_id integer)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select animals_type.name from animals_type
-        inner join animals_sort on animals_type.animals_type_id = animals_sort.animals_type_id
-        where animals_sort.animals_sort_id = sort_id);
     begin
-        return name;
+        return query select animals_type.animals_type_id, animals_type.name from animals_type
+        inner join animals_sort on animals_type.animals_type_id = animals_sort.animals_type_id
+        where animals_sort.animals_sort_id = sort_id;
     end;
 $$ language plpgsql;
 
 drop function get_animal_type_by_animal_sort_name(text);
 create or replace function get_animal_type_by_animal_sort_name(sort_name text)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select animals_type.name from animals_type
-        inner join animals_sort on animals_type.animals_type_id = animals_sort.animals_type_id
-        where animals_sort.name = sort_name);
     begin
-        return name;
+        return query select animals_type.animals_type_id, animals_type.name from animals_type
+        inner join animals_sort on animals_type.animals_type_id = animals_sort.animals_type_id
+        where animals_sort.name = sort_name;
     end;
 $$ language plpgsql;
 ------------------------------------------------------------------------------------------------------------------------
 drop function get_plant_type_by_type_id(integer);
 create or replace function get_plant_type_by_type_id(type_id integer)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select plants_type.name from plants_type where plants_type_id = type_id);
     begin
-        return name;
+        return query select * from plants_type where plants_type_id = type_id;
     end;
 $$ language plpgsql;
 
 
 drop function get_plant_type_by_plant_id(integer);
 create or replace function get_plant_type_by_plant_id(plant_id integer)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select plants_type.name from plants_type
+    begin
+        return query select plants_type.plants_type_id, plants_type.name from plants_type
         inner join plants_sort on plants_type.plants_type_id = plants_sort.plants_type_id
         inner join plants on plants.plants_sort_id = plants_sort.plants_sort_id
-        where  plants.plants_id = plant_id);
-    begin
-        return name;
+        where plants.plants_id = plant_id;
     end;
 $$ language plpgsql;
 
 
 drop function get_plant_type_by_plant_name(text);
 create or replace function get_plant_type_by_plant_name(plant_name text)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select plants_type.name from plants_type
+    begin
+        return query select plants_type.plants_type_id, plants_type.name from plants_type
         inner join plants_sort on plants_type.plants_type_id = plants_sort.plants_type_id
         inner join plants on plants.plants_sort_id = plants_sort.plants_sort_id
-        where  plants.name = plant_name);
-    begin
-        return name;
+        where  plants.name = plant_name;
     end;
 $$ language plpgsql;
 
 
 drop function get_plant_type_by_plant_sort_id(integer);
 create or replace function get_plant_type_by_plant_sort_id(sort_id integer)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select plants_type.name from plants_type
-        inner join plants_sort on plants_type.plants_type_id = plants_sort.plants_type_id
-        where plants_sort.plants_sort_id = sort_id);
     begin
-        return name;
+        return query select plants_type.plants_type_id, plants_type.name from plants_type
+        inner join plants_sort on plants_type.plants_type_id = plants_sort.plants_type_id
+        where plants_sort.plants_sort_id = sort_id;
     end;
 $$ language plpgsql;
 
 drop function get_plant_type_by_plant_sort_name(text);
 create or replace function get_plant_type_by_plant_sort_name(sort_name text)
-returns text as
+returns table (id integer, name text) as
 $$
-    declare
-        name text := (select plants_type.name from plants_type
-        inner join plants_sort on plants_type.plants_type_id = plants_sort.plants_type_id
-        where plants_sort.name = sort_name);
     begin
-        return name;
+        return query select plants_type.plants_type_id, plants_type.name from plants_type
+        inner join plants_sort on plants_type.plants_type_id = plants_sort.plants_type_id
+        where plants_sort.name = sort_name;
     end;
 $$ language plpgsql;
 -- todo: add more functions
